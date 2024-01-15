d8f258568a1ec328d2dfab8f48848e856914d988
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-15 05:43:02 +0000

    Updated

---
+++ .devcontainer/01_basics/01_Variables.js
@@ -0,0 +1,13 @@
+const accountId = 144553
+let accountEmail = "aakanshatidke@gmail.com"
+var accountPassword = "12345"
+accountCity = "Nagpur"
+let accountState;
+// accountId = 2 not allowed
+
+accountEmail = "hc@gmail.com";
+console.table({accountId,accountEmail,accountPassword,accountEmail});
+
+/* var does not use frequently due to its scope kyuki apn ek jgh chnage karege to sb jgh chnage kr deta hai */
+/* let isme aisa koi scope wala scene nhi hai */
+/* prefer not to use 'var' because of issue in block scope & functional scope */
