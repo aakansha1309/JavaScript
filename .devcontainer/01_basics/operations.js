a620f575d4b3626c04e84156518befd148c24de7
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-16 06:20:49 +0000

    operations

---
+++ .devcontainer/01_basics/operations.js
@@ -0,0 +1,40 @@
+let value = 3;
+let negValue = -3;
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2) //power
+// console.log(2/2)
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " Aakansha"
+let str3 = str1 + str2;
+//console.log(str3);
+
+// console.log("1" + 2)
+// console.log("1" + "2")
+// console.log("1" + 2 +2) // agr string 1st hai to sbhi lo string kitrh treat kiya jayega aur string last hai to jo aagee to opeartion hai vo hoga phir string add hogi
+// console.log(1 + 2 + "2")
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+// increment(++)
+
+let x = 3;
+const y = x++;
+console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a;
+console.log(`a:${a}, b:${b}`);
