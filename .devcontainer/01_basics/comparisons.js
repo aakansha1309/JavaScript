8590189a36fa8c1f67441ac0b4aea2c5cd68de98
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-16 06:53:56 +0000

    updated

--- .devcontainer/01_basics/comparisons.js
+++ .devcontainer/01_basics/comparisons.js
@@ -8,14 +8,14 @@
 // console.log("2" > 1);  // not a good practice datatypes should be same
 // console.log("02" > 1);
 
-// console.log(null > 0);
-// console.log(null == 0);
-// console.log(null >= 0);  //the reason is that an equality check == and comparisons > <= <= work differently. Comparisons convert null to a number treating it as 0. Thats why null >= 0 is true & null >= 0 is false
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);  //the reason is that an equality check == and comparisons > <= <= work differently. Comparisons convert null to a number treating it as 0. Thats why null >= 0 is true & null > 0 is false
 
-console.log(undefined == 0)
-console.log(undefined >= 0)
-console.log(undefined <= 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
 
-// === strict check datatype bhi check karega
-console.log(2 === 2);
-console.log("2" === 2);
+// // === strict check datatype bhi check karega
+// console.log(2 === 2);
+// console.log("2" === 2);
