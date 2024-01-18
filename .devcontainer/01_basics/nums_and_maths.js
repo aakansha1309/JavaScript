ca52c9616f8b561fbfc40a5065ad506dfca7e0d0
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-18 10:02:18 +0000

    math & num

---
+++ .devcontainer/01_basics/nums_and_maths.js
@@ -0,0 +1,28 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+//console.log(balance);
+
+const otherNumber = 23.8966;
+//console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+//console.log(hundreds.toLocaleString('en-IN'));
+
+/// ++++++++++++++++MATHS++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.ceil(4.6));  
+// console.log(Math.floor(4.2));
+// console.log(Math.max(4,3,5,9));
+// console.log(Math.min(4,3,5,9));
+
+console.log(Math.random()); // lies bet 0-1
+console.log((Math.random()*10) + 1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
