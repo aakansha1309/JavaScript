ead040062a6a5f55de454f378cec5af8c27e2ca6
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-20 15:35:07 +0000

    Scope

---
+++ .devcontainer/03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+//var c = 300 // global scope
+
+let a = 300
+
+if (true ) {   // block scope
+    let a = 10
+    const b = 20
+    console.log("INNER: ",a);
+}
+
+console.log(a)
+//console.log(b)
+//console.log(c);
