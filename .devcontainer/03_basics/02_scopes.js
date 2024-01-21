81b4a7335903cc9249ff221d268c9353cb584137
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-21 05:16:38 +0000

    scopes in detail

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -5,9 +5,49 @@ let a = 300
 if (true ) {   // block scope
     let a = 10
     const b = 20
-    console.log("INNER: ",a);
+    //console.log("INNER: ",a);
 }
 
-console.log(a)
+// console.log(a)
 //console.log(b)
 //console.log(c);
+
+// nested scope
+
+function one() {   // one can access two but not vice versa  -- child can access parent function
+    const username = "Aakansha"
+
+    function two() {
+        const website = "Youtube"
+        console.log(username)
+    }
+    //console.log(website);
+
+        two()
+}
+
+        //one()
+
+        if(true) {
+            const username = "Aakansha"
+        if(username === "Aakansha") {
+            const website = " Youtube" 
+           // console.log(username + website);
+        }
+        //console.log(website)
+        }
+        //console.log(username)
+
+///// ++++++++++++++++interesting+++++++++++++++++++++++++++
+console.log(addone(5))
+
+function addone(num) {
+    return num + 1
+}
+
+
+addTwo(5)   /// hoisting ths cannnot be done before execution
+const addTwo = function(num) {
+    return num + 2
+}
+
