e6e88798f9e4e02d2e690db495297749ad73d63f
aakansha1309 <135333180+aakansha1309@users.noreply.github.com>
2024-01-28 06:23:22 +0000

    commit

--- .devcontainer/05_iterations/three.js
+++ .devcontainer/05_iterations/three.js
@@ -11,6 +11,28 @@ for (const num  of arr) {
 
 const greetings = "Hello World!!"
 for (const greet of greetings) {
-     console.log(`Each char is ${greet}`);
+    // console.log(`Each char is ${greet}`);
 }
     
+// map 
+
+const map = new Map()
+map.set('IN' , "INDIA")
+map.set('Fr' , "France")
+map.set('USA' , "UNITED STATES")
+
+console.log(map);
+
+for (const [key, value] of map) {
+   console.log(key,':-', value);
+}
+
+const myObject = {
+   'game1': 'NFS',
+   'game2': 'Spiderman'
+}
+
+// for (const [key,value] of myObject) {
+//    console.log(key, ':-', value);
+// }
+
